b1bc16ad1b54d732d3442b3c5f4ac8bdd55fd115
hrridoyv2 <hrridoyv2@gmail.com>
2021-01-28 03:07:58 +0600

    deploy

--- index.js
+++ index.js
@@ -42,6 +42,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port || process.env.PORT, () => {
-  console.log(`Example app listening at ${port || process.env.PORT}`);
-});
+app.listen(port || process.env.PORT);
