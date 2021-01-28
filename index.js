0a2d5af46d5067e8f12dcd211aa2117fcdefdb40
hrridoyv2 <hrridoyv2@gmail.com>
2021-01-28 16:42:40 +0600

    updated

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xya9j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express());
 const port = 5000;
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pea2o.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xya9j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 client.connect((err) => {
   const buy = client.db("african-finex").collection("buy");
   const sell = client.db("african-finex").collection("sell");
@@ -28,7 +26,6 @@ client.connect((err) => {
 
   app.post("/buy", (req, res) => {
     buy.insertOne(req.body).then((result) => {
-      console.log(result);
       res.send(result.insertedCount > 0);
     });
   });
@@ -43,7 +40,7 @@ client.connect((err) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Application is working successfully");
 });
 
 app.listen(process.env.PORT);
